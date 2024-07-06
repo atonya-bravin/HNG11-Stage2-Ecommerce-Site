289b5a6ece92e4c895d4ede6fdfc5e2f077cb690
atonya-bravin <bravinatonya.brani@gmail.com>
2024-07-07 02:39:06 +0300

    Re-structuring of the application.
    The re-structuring has been done in order to seperate different
    functionalities from each other. The basic structures implemented are;
    	- Components
    	- Contexts
    	- Styles
    	- Tests
    	- Index file
    
    see: #4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './Components/App';
+import reportWebVitals from './Tests/reportWebVitals';
 import "semantic-ui-css/semantic.min.css";
+import './Styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
