9bd9bdc61bdb01ecb0f99113964624173a87b2ec
atonya-bravin <bravinatonya.brani@gmail.com>
2024-07-07 04:23:06 +0300

    Inclusion of the FlashSale component in the application.
    The FlashSale component is the one that holds images of products on
    flash sale.
    
    see: #5

--- src/Components/OnlineShop.js
+++ src/Components/OnlineShop.js
@@ -3,10 +3,12 @@
  *  all the other components within the application. 
 */
 
+import FlashSale from "./FlashSale";
+
 const OnlineShop = () => {
     return(
         <div className="ui container">
-            
+            <FlashSale />
         </div>
     );
 };
