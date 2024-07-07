fdfe03112004f5cb7caf8c77ec959707729d2e34
atonya-bravin <bravinatonya.brani@gmail.com>
2024-07-07 04:28:29 +0300

    Creation of the FlashSale Component.
    This component holds the images of products on flash sale.
    
    see: #6

---
+++ src/Components/FlashSale.js
@@ -0,0 +1,23 @@
+/*
+    This Component holds the images representing items on flash sale.
+*/
+
+const FlashSale = () => {
+    return(
+        <div>
+            <h5 className="font-bold text-[18px] py-[8px]">
+                EXCLUSIVE PERKS
+            </h5>
+            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-8">
+                <div className="h-[50vh] flashSale rounded-[8px]" id='flashSale-1'>
+                    
+                </div>
+                <div className="h-[50vh] flashSale hidden md:inline rounded-[8px]" id='flashSale-2'>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FlashSale;
